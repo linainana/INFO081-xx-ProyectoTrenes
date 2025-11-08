de3caf6f13130e9a013cbb7edd8acce421620772
Escarlett <escarlett.vargas.a@gmail.com>
2025-11-08 20:22:45 -0300

    Update estado_de_simulacion.py

--- logic/estado_de_simulacion.py
+++ logic/estado_de_simulacion.py
@@ -3,3 +3,31 @@ from models.estacion import Estacion
 from models.ruta import Ruta
 
 class EstadoSimulacion:
+    def __init__(self):
+        self.hora_actual = "07:00"
+        self.trenes = []
+        self.estaciones = []
+        self.rutas = []
+        self.eventos = []
+
+    def estado_inicial_simulacion(self):
+        self.trenes = [
+            Tren("Tren BMU", 160, 236),
+            Tren("Tren EMU – EFE SUR", 120, 180)
+        ]
+
+        self.estaciones = [
+            Estacion("Estación Central", "RM", 8242459),
+            Estacion("Rancagua", "O’Higgins", 274407)
+        ]
+
+        self.rutas = [
+            Ruta("Estación Central", "Rancagua", 87)
+        ]
+
+    def tiempo_avanzado(self, minutos):
+        print(f"Ha avanzando {minutos} minutos...")
+
+    def registrao_de_evento(self, descripcion):
+        self.eventos.append(descripcion)
+        print(f"Nuevo evento registrado: {descripcion}")
