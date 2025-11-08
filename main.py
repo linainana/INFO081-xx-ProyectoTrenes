63034ea1ee44895118fbe3beac47e0e831c1002e
Isidora Villagrán <isidoravillagran23@gmail.com>
2025-11-08 20:37:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-ola
+import tkinter as tk
+import ui
+
+def main():
+    try:
+        ui
+    except Exception as i:
+        print(f"Error al iniciar la interfaz: {i}")
+
+
+
+if __name__ == "__main__":
+    main()
