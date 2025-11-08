04d49e78446be94da749e6731961a822abe6d818
nissydva <escarlett.vargas.a@gmail.com>
2025-11-08 18:08:39 -0300

    Update estaciones.py

--- models/estaciones.py
+++ models/estaciones.py
@@ -1,1 +1,13 @@
+class Estacion:
+    def __init__(self, nombre, region, poblacion):
+        self.nombre = nombre
+        self.region = region
+        self.poblacion = poblacion
+        self.conexiones = []  
 
+    def agregar_conexion(self, estacion_destino):
+        if estacion_destino not in self.conexiones:
+            self.conexiones.append(estacion_destino)
+
+    def descripcion_estacion(self):
+        return f"{self.nombre} ({self.region}) - Población: {self.poblacion}"
