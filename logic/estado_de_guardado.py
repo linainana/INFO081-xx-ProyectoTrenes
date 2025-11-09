5021f8dd82d0ca94e71fb9494cd8df731488078e
Cata <cata.provoste.c@gmail.com>
2025-11-08 21:30:45 -0300

    Update estado_de_guardado.py

--- logic/estado_de_guardado.py
+++ logic/estado_de_guardado.py
@@ -3,5 +3,12 @@ import os
 
 class EstadoGuardado:
 
-  def
+  def __init__(self, ruta_archivo="config/estado_guardado.json"):
+    self.ruta_archivo = ruta_archivo
+
+  def guardar(self, estado):
+    try:
+      carpeta=os.path.dirname(self.ruta_archivo)
+
+
 
